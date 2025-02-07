48b27557b82a362a2b9d94023984d9be6df5d94c
RangaireAonair <newBeePlus@outlook.pt>
2025-02-07 23:42:17 +0800

    feat: build minify

---
+++ postcss.config.cjs
@@ -0,0 +1,19 @@
+const { purgeCSSPlugin } = require('@fullhuman/postcss-purgecss');
+module.exports = {
+  plugins: [
+    purgeCSSPlugin({
+      content: ['**/*.html', '**/*.css', '**/*.scss', '**/*.vue', '**/*.jsx'],
+      safelist: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /data-v-.*/
+      ],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+      },
+      skippedContentGlobs: ['node_modules/**']
+    })
+  ]
+};
--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,64 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import createZipPlugin from './src/plugins/zip.js';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import viteCompression from 'vite-plugin-compression';
+import externalGlobals from 'rollup-plugin-external-globals';
 import { name } from './package.json';
 
 // https://vite.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [vue(), createZipPlugin({ packageName: name })],
+  plugins: [
+    vue(),
+    createZipPlugin({ packageName: name }),
+    viteCompression({
+      /** 是否在控制控台打印被 压缩的文件信息 */
+      verbose: false,
+      /** 是否禁用压缩、默认为false */
+      disable: false,
+      /** 启用压缩的文件大小限制，单位是字节，默认为0 */
+      threshold: 10240,
+      ext: '.gz',
+      algorithm: 'gzip',
+      deleteOriginFile: true
+    }),
+    createHtmlPlugin({
+      minify: false,
+      entry: 'src/main.js',
+      inject: {
+        data: {
+          vueScript: `<script src='https://cdn.jsdelivr.net/npm/vue@3.5.13/dist/vue.global.prod.js'></script>`
+        }
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
     },
     extensions: ['.js', '.jsx']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
+        entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
+        assetFileNames: '[ext]/[name]-[hash].[ext]' // 资源文件像 字体，图片等
+      },
+      external: ['Vue']
+    },
+    minify: 'esbuild',
+    sourceMap: false,
+    reportCompressedSize: false,
+    minifyCSS: true,
+    plugins: [
+      externalGlobals({
+        vue: 'Vue'
+      })
+    ]
+  },
+  esbuild: {
+    drop: ['debugger', 'console']
   }
 });
