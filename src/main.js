a5c10a3097e039d446d5b8b89066865420c942b6
RangaireAonair <newBeePlus@outlook.pt>
2025-02-11 23:18:05 +0800

    perf: remove Some Code

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import '@/style.css';
 import App from '@/App.vue';
 import { usePlugin } from '@/plugins/usePlugins';
-import 'element-plus/theme-chalk/index.css';
 const Instance = createApp(App);
 
 usePlugin(Instance);
