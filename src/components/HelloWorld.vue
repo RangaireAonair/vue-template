678079748e6f60aaa96b64bf619658934c630940
RangaireAonair <newBeePlus@outlook.pt>
2025-02-11 23:23:29 +0800

    test: add useModal Hooks Test Use-Cases

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,19 +2,36 @@
 import { ref, h } from 'vue';
 import { useModal } from '@/hooks/useModal.js';
 // import { ElButton } from 'element-plus';
+import testComponent from './testCom.vue';
 
 defineOptions({
   inheritAttrs: false
 });
 const showModal = () => {
-  useModal(h('div', null, [h('div', null, 'this is custom content')]), {}, { title: 'HelloWorld' });
+  const Component = h('div', null, [h('div', null, 'this is custom content')]);
+  useModal(Component, { title: 'Hello RangCli' });
+};
+const showInfoModal = () => {
+  const Component = h('div', null, [h('div', null, 'this is custom content')]);
+  useModal(
+    Component,
+    { title: 'HelloWorld' },
+    {
+      // Footer: true,
+      // FooterProps: {
+      //   confirmClick: () => console.log('confirm'),
+      //   cancelClick: () => console.log('Cancel')
+      // },
+      customFooter: testComponent
+    }
+  );
 };
 </script>
 
 <template>
   <el-button type="primary" @click="showModal">use Modal</el-button>
-  <el-button type="info" @click="showModal" :loading="true">use Modal Info</el-button>
-  <el-button>Default</el-button>
+  <el-button type="info" @click="showInfoModal">use Modal Info</el-button>
+  <el-button loading>Default</el-button>
 </template>
 
 <style scoped>
