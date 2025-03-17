ffed7499e977d6177cdb320d64556c5bbc16068d
RangaireAonair <newBeePlus@outlook.pt>
2025-03-17 10:52:18 +0800

    feat: add element-plus init control

--- src/plugins/usePlugins.js
+++ src/plugins/usePlugins.js
@@ -1,8 +1,9 @@
 import elementPlus from 'element-plus';
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
 export function usePlugin(App) {
-  App.use(elementPlus);
+  App.use(elementPlus, { locale: zhCn });
   for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     App.component(key, component);
   }
